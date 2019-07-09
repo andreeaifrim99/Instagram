2ee7afe4ddb7bc3041e41b3efdb831f0fa317d14
Andreea Ifrim <andreeaifrim@fb.com>
2019-07-08 17:50:48 -0700

    able to logout

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -1,21 +1,41 @@
 package com.example.instagram;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.instagram.model.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+    private Button logOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        logOutButton = findViewById(R.id.btnLogOut);
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if clicked, the user should no longer be remembered
+                ParseUser.logOut();
+                //user should be taken back to the login screen
+                final Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
         final Post.Query postsQuery = new Post.Query();
         postsQuery.getTop().withUser();
