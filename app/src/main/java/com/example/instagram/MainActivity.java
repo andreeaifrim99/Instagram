d84f51fd2cc6efa36abff3b3256967f829a03889
Andreea Ifrim <andreeaifrim@fb.com>
2019-07-09 11:50:24 -0700

    sign up from screen

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -7,16 +7,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText usernameInput;
     private EditText passwordInput;
     private Button loginBtn;
+    private Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         usernameInput = findViewById(R.id.etUsername);
         passwordInput = findViewById(R.id.etPassword);
         loginBtn = findViewById(R.id.loginBtn);
+        signUpBtn = findViewById(R.id.btnSignUp);
 
         //if user is already logged in then take them directly to the homepage
         ParseUser currentUser = ParseUser.getCurrentUser();
@@ -43,6 +47,37 @@ public class MainActivity extends AppCompatActivity {
                     final String password = passwordInput.getText().toString();
 
                     login(username, password);
+                }
+            });
+
+            signUpBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //open a new activity that will allow the user to sign
+                    //final Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                    //startActivity(intent);
+                    //finish();
+                    Toast.makeText(MainActivity.this, "Add username and password", Toast.LENGTH_LONG).show();
+                    final String username = usernameInput.getText().toString();
+                    final String password = passwordInput.getText().toString();
+
+                    ParseUser user = new ParseUser();
+                    user.setUsername(username);
+                    user.setPassword(password);
+
+                    user.signUpInBackground(new SignUpCallback() {
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                // Hooray! Let them use the app now
+                                login(username, password);
+                            } else {
+                                // Sign up didn't succeed. Look at the ParseException
+                                // to figure out what went wrong
+                                Log.e("LoginActivity", "Sign Up failure");
+                                e.printStackTrace();
+                            }
+                        }
+                    });
                 }
             });
         }
