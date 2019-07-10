c6dd549ad9b9c4614a033a067f88f22fc99fd9e2
Andreea Ifrim <andreeaifrim@fb.com>
2019-07-09 17:13:01 -0700

    multiple fragments

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -11,6 +13,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.instagram.fragments.ComposeFragment;
+import com.example.instagram.fragments.PostsFragment;
 import com.example.instagram.model.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -27,6 +31,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
 
         logOutButton = findViewById(R.id.btnLogOut);
         newPostButton = findViewById(R.id.btnCreate);
@@ -57,22 +63,28 @@ public class HomeActivity extends AppCompatActivity {
         bottomView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
                 switch (item.getItemId()) {
                     case R.id.action_home:
+                        fragment = new PostsFragment();
                         Toast.makeText(HomeActivity.this, "Home!", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_compose:
+                        fragment = new ComposeFragment();
                         Toast.makeText(HomeActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_profile:
                     default:
+                        fragment = new ComposeFragment();
                         Toast.makeText(HomeActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
                         break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
-
+        // Set default selection
+        bottomView.setSelectedItemId(R.id.action_home);
         loadTopPosts();
     }
 
