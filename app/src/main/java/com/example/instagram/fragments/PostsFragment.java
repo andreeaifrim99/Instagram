5169b111886efd7a23a61a8327c7d066d9a912c7
Andreea Ifrim <andreeaifrim@fb.com>
2019-07-09 17:51:59 -0700

    recycler view

--- app/src/main/java/com/example/instagram/fragments/PostsFragment.java
+++ app/src/main/java/com/example/instagram/fragments/PostsFragment.java
@@ -4,13 +4,27 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.instagram.PostsAdapter;
 import com.example.instagram.R;
+import com.example.instagram.model.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PostsFragment extends Fragment {
+    private RecyclerView rvPosts;
+    private PostsAdapter adapter;
+    private List<Post> mPosts;
 
     //onCreate view to inflate the view
 
@@ -18,5 +32,62 @@ public class PostsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        rvPosts = view.findViewById(R.id.rvPosts);
+
+
+        //create data source
+        mPosts = new ArrayList<>();
+        //create adapter
+        adapter = new PostsAdapter(getContext(), mPosts);
+        //set adapter on recycler view
+        rvPosts.setAdapter(adapter);
+        //set layout manager on recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        loadTopPosts();
+    }
+
+    //USED ANOTHER METHOD, THIS ONE MIGHT BE WRONG
+    private void loadTopPosts() {
+        final Post.Query postsQuery = new Post.Query();
+        postsQuery.getTop().withUser();
+        postsQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); ++i) {
+                        Log.d("HomeActivity", "Post[" + i + "] = " + objects.get(i).getDescription()
+                                +"\nusername = " + objects.get(i).getUser().getUsername());
+                    }
+
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> postQuery = new ParseQuery<Post>(Post.class);
+        postQuery.include(Post.KEY_USER);
+        postQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e("PostsFragment", "Error with query");
+                    e.printStackTrace();
+                    return;
+                }
+                mPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+                for (int i = 0; i < posts.size(); i++) {
+                    Post post = posts.get(i);
+                }
+            }
+        });
     }
 }
